5ce8f62632791c95187ed8060b0fd73e8ff4fa08
Omaenoshi <71465546+Omaenoshi@users.noreply.github.com>
2022-09-05 10:49:21 +0500

    added list of posts

---
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -0,0 +1,19 @@
+package com.example.demoweb.model;
+
+public class Post {
+
+    private final String text;
+    private int likes;
+
+    public Post(String text) {
+        this.text = text;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
