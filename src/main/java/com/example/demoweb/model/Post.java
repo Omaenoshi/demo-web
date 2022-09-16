16f186ff1f4011e319f74d49390f7d186d593bc4
Omaenoshi <71465546+Omaenoshi@users.noreply.github.com>
2022-09-16 21:19:19 +0500

    added bootstrap

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,12 +1,21 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
 
     private final String text;
     private int likes;
 
+    private final Date creationDate;
+
     public Post(String text) {
         this.text = text;
+        creationDate = new Date();
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 
     public int getLikes() {
